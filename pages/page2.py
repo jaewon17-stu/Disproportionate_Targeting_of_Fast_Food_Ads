88117024a44dc7ed90f247b6f283595bfbf93630
jaewon17-stu <jaewonkim0315@gmail.com>
2024-12-27 17:34:08 +0000

    add page

---
+++ pages/page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("Relevant Policies")
+st.sidebar.markdown("Relevant Policies")
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("Community")
+st.sidebar.markdown("Community")
